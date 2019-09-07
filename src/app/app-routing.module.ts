a77442956879a0614f68be89e87384a08c993047
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-09-07 16:14:40 +0200

    Register home, entries listing and entry creation routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {EntriesComponent} from './entries/entries.component';
+import {StoreEntryComponent} from './store-entry/store-entry.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'entries',
+    component: EntriesComponent,
+  },
+  {
+    path: 'entries/create',
+    component: StoreEntryComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
