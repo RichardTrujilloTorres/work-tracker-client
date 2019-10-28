66e06bd70eda0cc75d36666477f85801b2c6c508
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-10-28 02:27:29 +0100

    Remove dummy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HomeComponent } from './home/home.component';
 import { EntriesComponent } from './entries/entries.component';
 import { StoreEntryComponent } from './store-entry/store-entry.component';
 import {HttpClientModule} from '@angular/common/http';
-import { DummyComponent } from './dummy/dummy.component';
 import { EditEntryComponent } from './edit-entry/edit-entry.component';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
@@ -21,7 +20,6 @@ import {ngxLoadingAnimationTypes, NgxLoadingModule} from 'ngx-loading';
     HomeComponent,
     EntriesComponent,
     StoreEntryComponent,
-    DummyComponent,
     EditEntryComponent,
     NavbarComponent,
     DashboardPageComponent,
