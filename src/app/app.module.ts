f6bdff96a0d1838819d27a1b3cf6dc66259c81bd
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-11-05 17:23:55 +0100

    Update app module
    - Add login component
    - Register interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { EntriesComponent } from './entries/entries.component';
 import { StoreEntryComponent } from './store-entry/store-entry.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { EditEntryComponent } from './edit-entry/edit-entry.component';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
@@ -16,6 +16,9 @@ import {ngxLoadingAnimationTypes, NgxLoadingModule} from 'ngx-loading';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ShowEntryComponent} from './components/show-entry/show-entry.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {ErrorInterceptor} from './services/http/interceptors/error.interceptor';
+import { LoginComponent } from './pages/login/login.component';
+import {JwtInterceptor} from './services/http/interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -27,6 +30,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     NavbarComponent,
     DashboardPageComponent,
     ShowEntryComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,7 +45,19 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     ReactiveFormsModule,
     NgbModule,
   ],
-  providers: [FormsModule],
+  providers: [
+      FormsModule,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
