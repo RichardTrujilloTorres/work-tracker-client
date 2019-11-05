2f8c4596cf5e7026575305215692f686b83a0b89
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-11-05 17:23:00 +0100

    Update routes
    - Apply route restrictions
    - Add login route
    - Set default redirect route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import {StoreEntryComponent} from './store-entry/store-entry.component';
 import {EditEntryComponent} from './edit-entry/edit-entry.component';
 import {DashboardPageComponent} from './pages/dashboard-page/dashboard-page.component';
 import {ShowEntryComponent} from './components/show-entry/show-entry.component';
+import {LoginComponent} from './pages/login/login.component';
+import {AuthGuard} from './services/http/guards/auth.guard';
 
 
 const routes: Routes = [
@@ -15,18 +17,30 @@ const routes: Routes = [
   {
     path: 'entries',
     component: EntriesComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'entries/create',
     component: StoreEntryComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'entries/:id',
     component: ShowEntryComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'entries/:id/edit',
     component: EditEntryComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
 
