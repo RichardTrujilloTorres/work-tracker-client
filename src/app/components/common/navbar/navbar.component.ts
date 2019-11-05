2ee40cd2c73627a2604b22036586bb47f878f0c8
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-11-05 17:24:37 +0100

    Update navbar
    - Add user related links

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../../../services/authentication/authentication.service';
+import {User} from '../../../models/user';
+import {NotificationService} from '../../../services/notifications/notification.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+      private auth: AuthenticationService,
+      private notificationsService: NotificationService,
+      private router: Router
+  ) {
+    const user = auth.currentUser();
+    if (user) {
+      this.user = user;
+    }
+  }
 
   ngOnInit() {
   }
 
+  login() {
+    this.router.navigate(['/login']);
+  }
+
+  logout() {
+    this.auth.logout();
+    this.notificationsService.success('You have been logged out!');
+    this.router.navigate(['/']);
+  }
 }
