b92fe257ce28b05db9d20d6c5ce177615e80489f
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-11-09 16:15:22 +0100

    Add commit types

---
+++ src/common/types/commits.ts
@@ -0,0 +1,25 @@
+export interface CommitResponseData {
+    data: CommitData;
+}
+
+export interface CommitsResponseData {
+    data: CommitsData;
+}
+
+export interface CommitData {
+    commit: Commit;
+}
+
+export interface CommitsData {
+    commits: Commit[];
+}
+
+export interface Commit {
+    id: number;
+    branch: string;
+    commitsNumber: number;
+    date: Date;
+    entry: number;
+    repository: string;
+}
+
