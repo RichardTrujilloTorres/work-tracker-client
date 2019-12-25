186c81c76feb009d86e6b011d9571212dbfc9f7b
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-12-25 20:08:35 +0100

    Add profile types

--- src/common/types.ts
+++ src/common/types.ts
@@ -4,12 +4,20 @@ export interface ResponseData {
     data: object;
 }
 
+export interface ProfileResponseData {
+    data: ProfileData;
+}
+
 export interface EntryResponseData {
     data: EntryData;
 }
 
 export interface EntriesResponseData {
     data: EntriesData;
+}
+
+export interface ProfileData {
+    profile: Profile;
 }
 
 export interface EntryData {
@@ -28,6 +36,10 @@ export interface Entry {
     commits: Commit[];
 }
 
+export interface Profile {
+    firstname?: string;
+    lastname?: string;
+}
 export interface Param {
     key: string;
     value: string;
