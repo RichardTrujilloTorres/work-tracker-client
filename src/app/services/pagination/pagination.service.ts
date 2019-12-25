2d42978fd8443477eeda17ba659373d136a96875
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-12-25 20:08:52 +0100

    Make pagination service injectable

--- src/app/services/pagination/pagination.service.ts
+++ src/app/services/pagination/pagination.service.ts
@@ -1,3 +1,8 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class PaginationService {
   constructor(public page: number, public perPage: number) { }
 
